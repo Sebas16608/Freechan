2579a7560200ac0fbbe517fe0452fbe3748b9d9e
Sebas16608 <Asebasrr444@gmail.com>
2025-08-15 18:20:29 -0600

    Creyendo que podria hacer mas cosas jajaja

--- backend/boards/views.py
+++ backend/boards/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from .serializers import BoardSerializer
 from .models import Board
 from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 class BoardViewSet(viewsets.ModelViewSet):
     queryset = Board.objects.all()
