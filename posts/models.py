8f12884af048153f9a1eb6c3b7b73bbe746760c4
Sebas16608 <Asebasrr444@gmail.com>
2025-08-23 22:33:41 -0600

    Rehaciendo el proyecto desde 0 por problemas con la API

---
+++ posts/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from threads.models import Thread
+# Create your models here.
+from django.db import models
+from threads.models import Thread
+# Create your models here.
+
+class Post(models.Model):
+    thread = models.ForeignKey(Thread, on_delete=models.CASCADE, related_name="posts")
+    contenido = models.TextField()
+    creado = models.DateTimeField(auto_now_add=True)
+    featured = models.BooleanField(default=False)
+    
+    class Meta:
+        verbose_name = "Post"
+        verbose_name_plural = "Posts"
---
+++ threads/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from boards.models import Board
+# Create your models here.
+
+class Thread(models.Model):
+    board = models.ForeignKey(Board, on_delete=models.CASCADE, related_name='board')
+    titulo = models.CharField(max_length=255)
+    contenido = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    feautered = models.BooleanField(default=False)
+    update = models.BooleanField(default=True)
+    featured = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = "thread"
+        verbose_name_plural = "threads"
+
+    def __str__(self):
+        return self.titulo
