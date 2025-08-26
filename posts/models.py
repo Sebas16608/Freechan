3b58d159dfe69542e43664214306726b0e0c014e
Sebas16608 <asebasrr444@gmail.com>
2025-08-26 05:14:44 -0600

    Cosas que se deberian guardar

--- posts/models.py
+++ posts/models.py
@@ -8,6 +8,7 @@ from threads.models import Thread
 class Post(models.Model):
     thread = models.ForeignKey(Thread, on_delete=models.CASCADE, related_name="posts")
     contenido = models.TextField()
+    imagen = models.ImageField(upload_to="media", null=True, blank=True)
     creado = models.DateTimeField(auto_now_add=True)
     featured = models.BooleanField(default=False)
     
